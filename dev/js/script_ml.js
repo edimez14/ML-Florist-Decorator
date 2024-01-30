f19783dd51729032cff6cb525033b7303220da20
edimez14 <edimez14@gmail.com>
2024-01-30 15:16:32 -0500

    modified the script_ml.js, style_ml.css, and index.html files by enhancing the images and adding a popup.

--- dev/js/script_ml.js
+++ dev/js/script_ml.js
@@ -1,3 +1,16 @@
+/*funcionamiento del popup*/
+document.querySelectorAll('#galeria img').forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector('.popup_img').style.display = 'block';
+        document.querySelector('.popup_img img').src = image.getAttribute('src');
+    }
+});
+
+document.querySelector('.popup_img button').onclick = () =>{
+    document.querySelector('.popup_img').style.display = 'none';
+};
+
+/*funcionamiento de la ventana modal*/
 const btnLanzarModal = document.querySelector('#lanzar_modal');
 const btnOcultarModal = document.querySelector('#ocultar_modal');
 
@@ -31,6 +44,7 @@ btnOcultarModal.addEventListener('click', (e) => {
     contacto.appendChild(pComentario);
 });
 
+/*funcionamiento del formulario*/
 document.addEventListener("DOMContentLoaded", function () {
     // Script para el formulario de contacto
     const formularioContacto = document.getElementById("formulario-contacto");
